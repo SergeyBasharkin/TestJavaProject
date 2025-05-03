d3cf7c1b00cf82143777aa670c9fd1b03db582f2
Sergei Basharkin <Basharkin@lanit.ru>
2025-05-03 10:46:13 +0300

    init

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World 22112321!");
+        System.out.println("Hello World 221asdasdaskd ask askl d12321!");
+        System.out.println("Hello World 221asdasdaskd ask askl d12321!");
+        System.out.println("Hello World 221asdasdaskd ask askl d12321!");
+        System.out.println("Hello World 221asdasdaskd ask askl d12321!");
     }
 }
